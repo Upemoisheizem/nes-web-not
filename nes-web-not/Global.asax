e13f8d6bb53bfd5763e7121953e7da67394a1b4f
w10 <w10.blah.com>
2024-05-30 09:17:48 +1000

    Add project files.

---
+++ nes-web-not/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="nes_web_not.MvcApplication" Language="C#" %>
---
+++ nes-web-not/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(nes_web_not.Startup))]
+namespace nes_web_not
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
